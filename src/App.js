5790fbd80a9ee5bbd98c8f8c5d383e53a89f444c
Vishal Kumar <vk3vishal@gmail.com>
2024-12-04 11:54:10 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import NavBar from './components/NavBar'
+import "./App.css"
+import { loadGrid } from './loadGrid';
+import Board from './components/Board';
+const API_ADDRESS = "https://api.quicksell.co/v1/internal/frontend-assignment";
 
 function App() {
+  
+  const [users, setUsers] = useState([]);
+  const [tickets, setTickets] = useState([]);
+  const [grouping, setGrouping] = useState("");
+  const [sorting, setSorting] = useState("");
+  const [grid, setGrid] = useState([]);
+
+  useEffect(() => {
+    // API  data fetch
+    const fetchData = async () => {
+      const response = await fetch(API_ADDRESS);
+      const data = await response.json();
+      setTickets(data.tickets);
+      setUsers(data.users);
+
+      // Load saved view state
+      const savedGrouping = localStorage.getItem("grouping") || "status";
+      const savedSorting = localStorage.getItem("sorting") || "priority";
+      setGrouping(savedGrouping);
+      setSorting(savedSorting);
+    };
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("sorting", sorting);
+    loadGrid(sorting, grouping, setGrid, tickets);
+
+    console.log(tickets, users);
+  }, [sorting, grouping]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <NavBar grouping = {grouping} sorting={sorting} setGrouping={setGrouping} setSorting={setSorting}/>
+      <Board grid={grid}/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Card.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Card({user, ticket}) {
+
+    const {username, available} = user;
+    const {id, title, tag, userId, status, priority} = ticket;
+
+  return (
+    <div className='card'>
+        
+    </div>
+  )
+}
+
+export default Card
